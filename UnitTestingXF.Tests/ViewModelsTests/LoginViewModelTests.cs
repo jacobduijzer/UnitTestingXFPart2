d145c09c92bc899ac18d5d236fb884f95fc2ac5e
Jacob Duijzer <jacob.duijzer@redhotminute.com>
2017-07-28 19:23:53 +0200

    Initial check in, copy of Part 1

---
+++ UnitTestingXF.Tests/ViewModelsTests/LoginViewModelTests.cs
@@ -0,0 +1,57 @@
+﻿﻿using System;
+using NUnit.Framework;
+using UnitTestingXF.ViewModels;
+
+namespace UnitTestingXF.Tests.ViewModelsTests
+{
+    [TestFixture]
+    public class LoginViewModelTests
+    {
+        [Test]
+        public void VMCanConstruct()
+        {
+            var vm = new LoginViewModel();
+            Assert.AreEqual(typeof(LoginViewModel), vm.GetType());
+        }
+
+        [Test]
+        public void CannotLoginWithEmptyFormTest()
+        {
+            var vm = new LoginViewModel();
+            Assert.IsFalse(vm.LoginCommand.CanExecute(null));
+        }
+
+        [Test]
+        public void CanLoginWithValidFormTest()
+        {
+            var vm = new LoginViewModel();
+            Assert.IsFalse(vm.IsFormValid);
+            Assert.IsFalse(vm.LoginCommand.CanExecute(null));
+
+            vm.Username = "testuser";
+            Assert.IsFalse(vm.IsFormValid);
+            Assert.IsFalse(vm.LoginCommand.CanExecute(null));
+
+            vm.Password = "testpassword";
+            Assert.IsFalse(vm.IsFormValid);
+            Assert.IsFalse(vm.LoginCommand.CanExecute(null));
+
+            vm.Username = string.Empty;
+            Assert.IsFalse(vm.IsFormValid);
+            Assert.IsFalse(vm.LoginCommand.CanExecute(null));
+
+            vm.Username = "     ";
+            Assert.IsFalse(vm.IsFormValid);
+            Assert.IsFalse(vm.LoginCommand.CanExecute(null));
+
+            vm.Password = "      ";
+            Assert.IsFalse(vm.IsFormValid);
+            Assert.IsFalse(vm.LoginCommand.CanExecute(null));
+
+            vm.Username = "test@test.com";
+            vm.Password = "test123!";
+            Assert.IsTrue(vm.IsFormValid);
+            Assert.IsTrue(vm.LoginCommand.CanExecute(null));
+        }
+    }
+}
---
+++ UnitTestingXF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MvvmHelpers;
+using Xamarin.Forms;
+using UnitTestingXF.Helpers;
+
+namespace UnitTestingXF.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => await DoLoginAsync(), () => IsFormValid);
+        }
+
+        public ICommand LoginCommand { get; private set; }
+
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set { SetProperty(ref _username, value); OnPropertyChanged("IsFormValid"); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); OnPropertyChanged("IsFormValid"); }
+        }
+
+        public bool IsFormValid
+        {
+            get => EmailValidator.IsValidEmail(Username) && PasswordValidator.IsValidPassword(Password);            
+        }
+
+		private async Task DoLoginAsync()
+		{
+            if(!IsBusy)
+            {
+                IsBusy = true;
+
+                // TODO: Implement login logic
+
+                IsBusy = false;
+            }
+		}
+    }
+}
